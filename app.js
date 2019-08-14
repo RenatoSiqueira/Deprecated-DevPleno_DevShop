b17f284a7ded5b8b86a0cee565081eaa5993a793
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-08-14 14:55:14 -0300

    Fixed Model Category

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const init = db => {
     const express = require('express')
     const app = express()
 
-    const categoryModel = require('./controllers/categories')
-
+    const category = require('./models/category')
     const routes = require('./routes')
 
     app.set('view engine', 'ejs')
     app.use(express.static('public'))
+
     app.use(async (req, res, next) => {
-        const categories = await categoryModel.getCategories(db)()
+        const categories = await category.getCategories(db)()
         res.locals = { categories }
         next()
     })
